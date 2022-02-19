58a9915c8a34a98f109366339c43b33833d8abdf
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-02-19 18:18:49 +0600

    done

--- leaflet.js
+++ leaflet.js
@@ -1,18 +1,22 @@
 // create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map').setView([23.8759, 90.3795], 16);
+var map = L.map('map').setView([23.876032, 90.369125], 18);
 
 var latlngs = [
-    [23.876104, 90.379106],
-    [23.875621, 90.378870],
-    [23.875008, 90.378591],
-    [23.874655, 90.378516],
-    [23.874055, 90.378416],
+    [23.875958, 90.369144],
+    [23.875691, 90.369074]
+
 ];
 
 var latlngs2 = [
-    [23.876149, 90.379050],
-    [23.876593, 90.378200],
-    [23.876703, 90.377513]
+    [23.876015, 90.369168],
+    [23.876248, 90.368446],
+    [23.875914, 90.368390],
+    [23.875654, 90.368484],
+    [23.875632, 90.368535],
+    [23.875519, 90.369016],
+    [23.875482, 90.369507],
+    [23.875852, 90.369555],
+    [23.876026, 90.369091]
 ];
 
 var latlngs3 = [
@@ -26,7 +30,7 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // add a marker in the given location, attach some popup content to it and open the popup
-L.marker([23.8759, 90.3795]).addTo(map)
+L.marker([23.876032, 90.369125]).addTo(map)
     .bindPopup('Uttara Banani. Water Logging. Red Line')
     .openPopup();
 
@@ -36,14 +40,14 @@ var polyline3 = L.polyline(latlngs3, { color: 'blue' });
 
 polyline.addTo(map);
 // create a map in the "map" div, set the view to a given place and zoom
-var map2 = L.map('map2').setView([23.8759, 90.3795], 16);
+var map2 = L.map('map2').setView([23.876032, 90.369125], 18);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributor'
 }).addTo(map2);
 
-L.marker([23.8759, 90.3795]).addTo(map2)
+L.marker([23.876032, 90.369125]).addTo(map2)
     .bindPopup('Uttara Banani. Alternative route. Blue Line')
     .openPopup();
 
