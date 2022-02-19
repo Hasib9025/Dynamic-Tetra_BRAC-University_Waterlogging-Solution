be98609af1c8f97182b7f22c16a3558e22ea9479
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-02-19 13:20:06 +0600

    buttons added

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-        var mymap = L.map('mapid').setView([28.612, 77.229], 13);
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
-        // making a marker
-        var marker = L.marker([28.614, 77.199]).addTo(mymap);
-        // making a circle on area
-        var circle = L.circle([28.612, 77.229], {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0.5,
-            radius: 1000
-        }).addTo(mymap);
-        // adding popup to marker
-        marker.bindPopup("<b>Welcome to Rashtrapati Bhawan</b>.").openPopup();
+var map1 = document.getElementById("map-1")
+var map2 = document.getElementById("map-2")
+
+document.getElementById("home").addEventListener("click", function() {
+    map1.style.display = "block";
+    map2.style.display = "none";
+});
+
+document.getElementById("alternate").addEventListener("click", function() {
+    map1.style.display = "none";
+    map2.style.display = "block";
+});
